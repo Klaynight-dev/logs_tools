4aef280a8071514f32893a0cd990253a069187d8
Klaynight <63601267+Klaynight-dev@users.noreply.github.com>
2024-12-10 17:28:41 +0100

    Update logs.py

--- logs.py
+++ logs.py
@@ -45,8 +45,8 @@ def log_action(action, success=True, error_message=None, empr=False):
 
     log_filename = get_log_filename()
     with open(log_filename, 'a') as log_file:
-        log_file.write(f"Timestamp: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Action: {action} - Success: {'Yes' if success else 'No'} - Error: {error_message}\n")
+        log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Action: {action} - Success: {'Yes' if success else 'No'} -","Error: {error_message}\n" if !success else "\n")
         if not success and error_message:
             log_file.write(f"\n")
     if empr==True:
-        print(f"Timestamp: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Action: {action} - Success: {'Yes' if success else 'No'} - Error: {error_message}")
+        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Action: {action} - Success: {'Yes' if success else 'No'} -","Error: {error_message}" if !success else "")
